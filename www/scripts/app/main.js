5012ce32cd85ed872f4ba335a5624a5eb68ad143
Christopher LaBauve <jiminychris@gmail.com>
2014-07-30 17:33:05 -0500

    Sprite animation!
    Maxim now just sort of walks in place on the left side of a tiled
    dungeon background. It’s still really messy, though. But it’s coming
    along!

--- www/scripts/app/main.js
+++ www/scripts/app/main.js
@@ -34,16 +34,28 @@ define(["jquery"], function($) {
         context.fillRect(0, 0, canvas.width, canvas.height);
         var img = new Image;
         img.onload = function() {
-            var wall = newCanvas(width-2*margin, height-2*margin);
-            wallContext = wall.getContext("2d");
             var tile = newCanvas(8, 8);
-            tile.getContext("2d").drawImage(img, 0, 0, 8, 8, 0, 0,
-                8, 8);
-
+            var wall = newCanvas(width-2*margin, height-2*margin);
             var walkAnimation = [];
+            function tick(i)
+            {
+                gameScreen = newCanvas(width, height);
+                ctx = gameScreen.getContext("2d");
+                ctx.drawImage(wall, margin, margin);
+                ctx.drawImage(walkAnimation[i], 0, wall.height-26);
+
+                context.drawImage(gameScreen, 0, 0, gameScreen.width, gameScreen.height,
+                    0, 0, gameScreen.width*scale, gameScreen.height*scale);
+
+                setTimeout(function() { tick((i+1)%4) }, 250);
+            }
+            wallContext = wall.getContext("2d");
+            tile.getContext("2d").drawImage(img, 0, 0, 8, 8, 0, 0, 8, 8);
+
             for (var i=0; i<4; i++)
             {
                 var sprite = newCanvas(26, 26);
+                sprite.getContext("2d").drawImage(img, 26*(i+1), 8, 26, 26, 0, 0, 26, 26);
                 
                 walkAnimation.push(sprite);
             }
@@ -51,9 +63,7 @@ define(["jquery"], function($) {
             for (var j=0; j<wall.height; j+=tile.height)
                 for (var i=0; i<wall.width; i+=tile.width)
                     wallContext.drawImage(tile, i, j);
-
-            context.drawImage(wall, 0, 0, wall.width, wall.height,
-                margin*scale, margin*scale, wall.width*scale, wall.height*scale);
+            tick(0);
         };
         img.src = "images/spritesheet.png";
     }
