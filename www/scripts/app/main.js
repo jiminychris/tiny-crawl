b480e0e70fa820faa4bd1f8174fcc2fe2d4d1c1e
Christopher LaBauve <jiminychris@gmail.com>
2014-08-01 09:16:35 -0500

    Throwing more sprites in
    To make it look pretty.

--- www/scripts/app/main.js
+++ www/scripts/app/main.js
@@ -10,8 +10,8 @@ define(["jquery"], function($) {
         context.imageSmoothingEnabled = false; /// future
         return canvas;
     }
-    var canvases = $("canvas#screen");
-    if (canvases.length != 1)
+    var screenDivs = $("div#screen");
+    if (screenDivs.length != 1)
     {
         // TO-DO error
     }
@@ -19,50 +19,75 @@ define(["jquery"], function($) {
     {
         var scale = 3;
         var margin = 1;
-        var canvas = canvases[0];
-        var width = canvas.width;
-        var height = canvas.height;
-        canvas.width = canvas.width*scale;
-        canvas.height = canvas.height*scale;
+        var canvasWidth = 88;
+        var canvasHeight = 31;
+        var width = canvasWidth - margin*2;
+        var height = canvasHeight - margin*2;
+        var screenDiv = screenDivs.first();
+        var canvas = newCanvas(canvasWidth*scale, canvasHeight*scale);
+        screenDiv.append(canvas);
         var context = canvas.getContext("2d");
-
-        context.webkitImageSmoothingEnabled = false;
-        context.mozImageSmoothingEnabled = false;
-        context.imageSmoothingEnabled = false; /// future
+        context.fillStyle = "#000000";
+        var gameScreen = newCanvas(width, height);
+        gameScreen.getContext("2d").fillStyle = "#000000";
 
         context.fillStyle = "#000000"
         context.fillRect(0, 0, canvas.width, canvas.height);
         var img = new Image;
         img.onload = function() {
-            var tile = newCanvas(8, 8);
-            var wall = newCanvas(width-2*margin, height-2*margin);
-            var walkAnimation = [];
+            var wallTile = newCanvas(8, 8);
+            var floorTile = newCanvas(24, 6);
+            var menuTile = newCanvas(8, 8);
+            var wall = newCanvas(width, height-6);
+            var floor = newCanvas(width, 6);
+            var menu = newCanvas(width, 8);
+            var maximAnimation = [];
+            var skeletonAnimation = [];
             function tick(i)
             {
-                gameScreen = newCanvas(width, height);
-                ctx = gameScreen.getContext("2d");
-                ctx.drawImage(wall, margin, margin);
-                ctx.drawImage(walkAnimation[i], 0, wall.height-26);
+                var ctx = gameScreen.getContext("2d");
+                ctx.fillRect(0, 0, width, height);
+                ctx.drawImage(wall, 0, 0);
+                ctx.drawImage(floor, 0, gameScreen.height-floor.height);
+                ctx.drawImage(maximAnimation[i], 0, gameScreen.height-26);
+                ctx.drawImage(skeletonAnimation[i], 52, gameScreen.height-26);
+                ctx.drawImage(menu, 0, 0);
 
+                context.fillRect(0, 0, canvas.width, canvas.height);
                 context.drawImage(gameScreen, 0, 0, gameScreen.width, gameScreen.height,
-                    0, 0, gameScreen.width*scale, gameScreen.height*scale);
+                    margin*scale, margin*scale, gameScreen.width*scale, gameScreen.height*scale);
 
                 setTimeout(function() { tick((i+1)%4) }, 250);
             }
-            wallContext = wall.getContext("2d");
-            tile.getContext("2d").drawImage(img, 0, 0, 8, 8, 0, 0, 8, 8);
+            wallTile.getContext("2d").drawImage(img, 0, 0, 8, 8, 0, 0, 8, 8);
+            floorTile.getContext("2d").drawImage(img, 16, 0, 24, 6, 0, 0, 24, 6);
+            menuTile.getContext("2d").drawImage(img, 8, 0, 8, 8, 0, 0, 8, 8);
 
             for (var i=0; i<4; i++)
             {
                 var sprite = newCanvas(26, 26);
                 sprite.getContext("2d").drawImage(img, 26*(i+1), 8, 26, 26, 0, 0, 26, 26);
                 
-                walkAnimation.push(sprite);
+                maximAnimation.push(sprite);
+            }
+            for (var i=0; i<4; i++)
+            {
+                var sprite = newCanvas(26, 26);
+                var ctx = sprite.getContext("2d");
+                ctx.translate(sprite.width, 0);
+                ctx.scale(-1, 1);
+                ctx.drawImage(img, 26*(i+1), 34, 26, 26, 0, 0, 26, 26);
+                
+                skeletonAnimation.push(sprite);
             }
 
-            for (var j=0; j<wall.height; j+=tile.height)
-                for (var i=0; i<wall.width; i+=tile.width)
-                    wallContext.drawImage(tile, i, j);
+            for (var j=wall.height-wallTile.height; j>-wall.height; j-=wallTile.height)
+                for (var i=0; i<wall.width; i+=wallTile.width)
+                    wall.getContext("2d").drawImage(wallTile, i, j);
+            for (var i=0; i<floor.width; i+=floorTile.width)
+                floor.getContext("2d").drawImage(floorTile, i, 0);
+            for (var i=0; i<menu.width; i+= menuTile.width)
+                menu.getContext("2d").drawImage(menuTile, i, 0);
             tick(0);
         };
         img.src = "images/spritesheet.png";
