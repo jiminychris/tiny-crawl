c5409efb0e24ac6d9730f886b9751b70fe5f97b4
Christopher LaBauve <jiminychris@gmail.com>
2014-08-08 16:03:57 -0500

    Just playing around with things

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,3 +1,12 @@
 var World = require("./ecs/world");
+var $ = require("jquery");
 
 var world = new World();
+
+var img = new Image();
+
+img.src = "images/spritesheet.png";
+
+$(document).ready(function() {
+    $("body").append(img);
+});
