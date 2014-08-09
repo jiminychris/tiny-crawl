9e2f549255c19164ce13da21e255fdb08f4ab807
Christopher LaBauve <jiminychris@gmail.com>
2014-08-08 22:02:03 -0500

    Implementing some ECS

--- www/scripts/ecs/world.js
+++ www/scripts/ecs/world.js
@@ -1,33 +1,75 @@
+var _ = require("underscore");
 var Exception = require("../exception");
 
 function World() {
-    this._deletedEntityStack = [];
-    this._entities = [];
+    this._entityCount = 0;
+    this._componentTypeCount = 0;
+
+    this._killedEntityStack = [];
+    this._components = [];
+    this._componentTypeRegistry = {};
+
     this._entityIdStream = function() {
         var id;
-        if (this._deletedEntityStack > 0)
-            id = this._deletedEntityStack.pop();
-        else
-            id = this._entities.length;
+        if (this._killedEntityStack > 0)
+            return this._killedEntityStack.pop();
+        id = this._entityCount;
+        this._entityCount += 1;
+        return id;
+    };
+    this._componentTypeIdStream = function() {
+        var id = this._componentTypeCount;
+        this._componentTypeCount += 1;
         return id;
     };
 }
 
 World.prototype.createEntity = function() {
-    var id = this._entityIdStream();
-    this._entities[id] = 1;
-    return id;
+    return this._entityIdStream();
 };
 
 World.prototype.killEntity = function(entity) {
-    this._deletedEntityStack.push(entity);
-    this._entities[entity] = null;
+    this._validateEntity(entity);
+    this._killedEntityStack.push(entity);
+    for (var i=0; i<this._componentTypeCount; i++)
+        this._components[i][entity] = null;
 };
 
 World.prototype.addComponent = function(entity, component) {
-    if (this._entities[entity] === null)
-        throw new Exception("entity '" + entity + "' does not exist");
-
+    this._validateEntity(entity);
+    this._components[this._getComponentId(component)][entity] = component;
 };
+
+World.prototype.fetchComponent = function(entity, componentName) {
+    this._validateEntity(entity);
+    try
+    {
+        this._validateComponentName(componentName);
+        return this._components[this._componentTypeRegistry[componentName]][entity];
+    } catch(e) {
+        return null;
+    }
+};
+
+World.prototype._validateEntity = function(entity) {
+    if (entity >= this._entityCount || _.contains(this._killedEntityStack, entity))
+        throw new Exception("entity '" + entity + "' does not exist");
+};
+
+World.prototype._validateComponentName = function(componentName) {
+    if (!_.has(this._componentTypeRegistry, componentName))
+        throw new Exception("component name '" + componentName + "' does not exist");
+};
+
+World.prototype._getComponentId = function(component) {
+    var name = component.constructor.componentName();
+    if (!_.has(this._componentTypeRegistry, name))
+    {
+        var id = this._componentTypeIdStream();
+        this._componentTypeRegistry[name] = id;
+        this._components[id] = [];
+    }
+    return this._componentTypeRegistry[name];
+}
 
 module.exports = World;
--- www/scripts/test/world.js
+++ www/scripts/test/world.js
@@ -1,20 +1,60 @@
 var assert = require("assert");
 var World = require("../ecs/world");
-describe('World', function() {
-    describe('#createEntity()', function() {
-        it('should return 0 and then 1', function() {
+var Exception = require("../exception");
+
+function Health() {};
+Health.componentName = function() {return "Health";};
+
+describe("World", function() {
+    describe("#createEntity()", function() {
+        it("should create unique IDs", function() {
             var world = new World();
-            assert.equal(0, world.createEntity());
-            assert.equal(1, world.createEntity());
+            var e0 = world.createEntity();
+            var e1 = world.createEntity();
+            assert.notEqual(e0, e1);
         });
-        it('should fill deleted spaces', function() {
+        it("should reuse deleted IDs", function() {
             var world = new World();
-            assert.equal(0, world.createEntity());
-            assert.equal(1, world.createEntity());
-            assert.equal(2, world.createEntity());
-            world.killEntity(1);
-            assert.equal(1, world.createEntity());
-            assert.equal(3, world.createEntity());
+            var e0 = world.createEntity();
+            var e1 = world.createEntity();
+            var e2 = world.createEntity();
+            world.killEntity(e1);
+            assert.strictEqual(e1, world.createEntity());
+        });
+    });
+    describe("#addComponent()", function() {
+        it("should throw an exception when referencing an undefined entity", function() {
+            var world = new World();
+            var health = new Health();
+            assert.throws(function() {
+                world.addComponent(0, health);
+            }, Exception);
+        });
+        it("should throw an exception when referencing a deleted entity", function() {
+            var world = new World();
+            var health = new Health();
+            var e0 = world.createEntity();
+            world.killEntity(e0);
+            assert.throws(function() {
+                world.addComponent(e0, health);
+            }, Exception);
+        });
+        it("should not throw an exception when used properly", function() {
+            var world = new World();
+            var health = new Health();
+            var e0 = world.createEntity();
+            assert.doesNotThrow(function() {
+                world.addComponent(e0, health);
+            }, Exception);
+        });
+    });
+    describe("#fetchComponent()", function() {
+        it("should fetch the proper component", function() {
+            var world = new World();
+            var health = new Health();
+            var e0 = world.createEntity();
+            world.addComponent(e0, health);
+            assert.strictEqual(health, world.fetchComponent(e0, Health.componentName()));
         });
     });
 });
